1678ffdc937e4ab53deb55000e8958009dc27586
Danil-Zaichkin <baziliopet@yandex.ru>
2023-03-20 23:55:40 +0500

    create Robot and Target classes

---
+++ robots/src/gui/Robot.java
@@ -0,0 +1,109 @@
+package gui;
+
+public class Robot {
+    private volatile double m_positionX;
+    private volatile double m_positionY;
+    private volatile double m_robotDirection = 0;
+    private static final double maxVelocity = 0.1;
+    private static final double maxAngularVelocity = 0.001;
+
+    Robot(double positionX, double positionY) {
+        m_positionX = positionX;
+        m_positionY = positionY;
+    }
+    private void moveRobot(double velocity, double angularVelocity, double duration)
+    {
+        velocity = applyLimits(velocity, 0, maxVelocity);
+        angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+        double newX = m_positionX + velocity / angularVelocity *
+                (Math.sin(m_robotDirection  + angularVelocity * duration) -
+                        Math.sin(m_robotDirection));
+        if (!Double.isFinite(newX))
+        {
+            newX = m_positionX + velocity * duration * Math.cos(m_robotDirection);
+        }
+        double newY = m_positionY - velocity / angularVelocity *
+                (Math.cos(m_robotDirection  + angularVelocity * duration) -
+                        Math.cos(m_robotDirection));
+        if (!Double.isFinite(newY))
+        {
+            newY = m_positionY + velocity * duration * Math.sin(m_robotDirection);
+        }
+        m_positionX = newX;
+        m_positionY = newY;
+        double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
+        m_robotDirection = newDirection;
+    }
+
+    protected void onModelUpdateEvent(double m_targetPositionX, double m_targetPositionY)
+    {
+        double distance = distance(m_targetPositionX, m_targetPositionY,
+                m_positionX, m_positionY);
+        if (distance < 0.5)
+        {
+            return;
+        }
+        double velocity = maxVelocity;
+        double angleToTarget = angleTo(m_positionX, m_positionY, m_targetPositionX, m_targetPositionY);
+        double angularVelocity = 0;
+        if (angleToTarget > m_robotDirection)
+        {
+            angularVelocity = maxAngularVelocity;
+        }
+        if (angleToTarget < m_robotDirection)
+        {
+            angularVelocity = -maxAngularVelocity;
+        }
+
+        moveRobot(velocity, angularVelocity, 10);
+    }
+
+    private static double applyLimits(double value, double min, double max)
+    {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+
+    private static double asNormalizedRadians(double angle)
+    {
+        while (angle < 0)
+        {
+            angle += 2*Math.PI;
+        }
+        while (angle >= 2*Math.PI)
+        {
+            angle -= 2*Math.PI;
+        }
+        return angle;
+    }
+
+    private static double angleTo(double fromX, double fromY, double toX, double toY)
+    {
+        double diffX = toX - fromX;
+        double diffY = toY - fromY;
+
+        return asNormalizedRadians(Math.atan2(diffY, diffX));
+    }
+
+    private static double distance(double x1, double y1, double x2, double y2)
+    {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    public double getPositionX() {
+        return m_positionX;
+    }
+
+    public double getPositionY() {
+        return m_positionY;
+    }
+
+    public double getDirection() {
+        return m_robotDirection;
+    }
+}
---
+++ robots/src/gui/Target.java
@@ -0,0 +1,27 @@
+package gui;
+
+import java.awt.Point;
+
+public class Target {
+    private volatile int m_positionX;
+    private volatile int m_positionY;
+
+    Target(int positionX, int positionY) {
+        m_positionX = positionX;
+        m_positionY = positionY;
+    }
+
+    protected void setPosition(Point p)
+    {
+        m_positionX = p.x;
+        m_positionY = p.y;
+    }
+
+    public int getPositionX() {
+        return m_positionX;
+    }
+
+    public int getPositionY() {
+        return m_positionY;
+    }
+}
